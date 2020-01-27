1e866afe77839243c6f1f15903c0afa3425ae2d2
Adam Darrah <piremies@gmail.com>
2020-01-27 16:35:31 -0500

    Nothing is working

--- src/DropkiqUI.ts
+++ src/DropkiqUI.ts
@@ -333,12 +333,6 @@ export class DropkiqUI {
     }
 
     this.boundElement.insertTextAtCaret(textToEnter);
-
-    var nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").set;
-    nativeInputValueSetter.call(this.element, 'react 16 value');
-    var ev2 = new Event('input', { bubbles: true});
-    this.element.dispatchEvent(ev2);
-
     this.closeMenu();
   };
 
